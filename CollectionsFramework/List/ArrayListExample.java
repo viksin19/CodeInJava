422cb471be7db27e213918de94a5a225a77b2d84
Singh, Vikram (vs192b) <vs192b@att.com>
2021-05-09 12:32:33 +0530

    Adding ArrayList Definition

--- CollectionsFramework/List/ArrayListExample.java
+++ CollectionsFramework/List/ArrayListExample.java
@@ -1,6 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/* ArrayList provides us with dynamic arrays in Java. 
+   Though, it may be slower than standard arrays but can be helpful in programs where lots of manipulation in the array is needed.
+   The size of an ArrayList is increased automatically if the collection grows or shrinks if the objects are removed from the collection. 
+   Java ArrayList allows us to randomly access the list. 
+   ArrayList can not be used for primitive types, like int, char, etc. We will need a wrapper class for such cases. */
+
 public class ArrayListExample {
 
     public static void main(String[] args) {
